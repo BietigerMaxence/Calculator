1289c7dbfb01f9ee71851a1a6b4759aba9f7eeb1
BietigerMaxence <bietigerm2@gmail.com>
2026-06-23 16:55:43 +0200

    Ajout de la vérification des saisies utilisateurs

--- main.c
+++ main.c
@@ -19,34 +19,48 @@ float division(float a, float b) {
 int main() {
     float num1, num2;
     char operator;
-    printf("Choose two numbers:\n");
-    if (scanf(" %f", &num1) != 1 || scanf(" %f", &num2) != 1) {
-        printf("Invalid numbers");
-        return 1;
-    }
+    char quit = 'n';
+    while (quit != 'y') {
 
-    printf("Choose one operator (+ * - /):\n");
-    if (scanf(" %c", &operator) != 1 || (operator != '+' && operator != '-' && operator != '*' && operator != '/')) {
-        printf("Invalid operator");
-        return 1;
-    }
+        printf("Choose two numbers:\n");
+        if (scanf(" %f", &num1) != 1 || scanf(" %f", &num2) != 1) {
+            printf("Invalid numbers\n");
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) { }
+            continue;
+        }
 
-    switch (operator) {
-        case '+':
-            printf("%.1f\n", addition(num1, num2));
-            break;
-        case '-':
-            printf("%.1f\n", subtraction(num1, num2));
-            break;
-        case '*':
-            printf("%.1f\n", multiplication(num1, num2));
-            break;
-        case '/':
-            if (num2 == 0) {
-                printf("Division by zero\n");
+        printf("Choose one operator (+ * - /):\n");
+        if (scanf(" %c", &operator) != 1 || (operator != '+' && operator != '-' && operator != '*' && operator != '/')) {
+            printf("Invalid operator\n");
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) { }
+            continue;
+        }
+
+        switch (operator) {
+            case '+':
+                printf("%.1f\n", addition(num1, num2));
                 break;
-            }
-            printf("%.1f\n", division(num1, num2));
-            break;
+            case '-':
+                printf("%.1f\n", subtraction(num1, num2));
+                break;
+            case '*':
+                printf("%.1f\n", multiplication(num1, num2));
+                break;
+            case '/':
+                if (num2 == 0) {
+                    printf("Division by zero\n");
+                    break;
+                }
+                printf("%.1f\n", division(num1, num2));
+                break;
+        }
+        printf("Quitter ? (y/n) : ");
+        if (scanf(" %c", &quit) != 1) {
+            printf("Invalid input\n");
+            return 1;
+        }
     }
+    return 0;
 }
